447b53acee2017a4e8c6568fc067dfb888a3c8f7
Alberto Duarte <betodute@gmail.com>
2019-03-11 17:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,3 @@ function happyHolidays() {
 function happyHolidaysTo(name){
   return "Happy holidays, " + name + "!";
 }
-
-function happyHolidaysTo(name, holiday){
-  return "Happy " + holiday + ", " + name + "!"
-}
